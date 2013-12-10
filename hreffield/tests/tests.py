122f3716bfa378361ea828236e54433cdd824ec5
Adam Alton <adamalton@gmail.com>
2013-12-10 00:05:15 +0000

    Adding skeleton files for tests.

---
+++ hreffield/tests/tests.py
@@ -0,0 +1,12 @@
+#LIBRARIES
+from django.test import TestCase
+
+#HREFFIELD
+from hreffield.tests.models import Link
+
+
+class HrefFieldTestCase(TestCase):
+    """ Tests for the HrefField. """
+
+    def test_x(self):
+        self.assertTrue(True)
